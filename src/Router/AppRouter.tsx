312acf3a8752392ada3b4ac3358b859ffa613949
Luis Acosta <lacostat@cjf.gob.mx>
2023-07-31 20:08:05 -0600

    Se elmina validacion para cambiar de titulo

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,20 +1,13 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { navegateList } from '../data/navegateData'
 import { RoutesInterface } from '../Interfaces/navigateInterface'
 import { Header } from '../components/Header'
-import { useEffect } from 'react'
 
 export const AppRouter = () => {
 
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    document.title = !pathname.includes('AppVerbs') ? `💻 Luizun` : 'Verb-Quest';
-  }, [location.pathname]);
-
   return (
     <>
-    { !pathname.includes('AppVerbs') && <Header />}
+    <Header />
     <Routes>
       {
         navegateList.map(({path, element}: RoutesInterface) => <Route path={path} element={element} key={path} />)
